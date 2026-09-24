de9d1a8485705d3f9eebffa27a023202935b6937
Virpitz <dmitri.prosvirov@mail.ru>
2026-09-24 09:35:24 +0300

    step7

--- server/program.cs
+++ server/program.cs
@@ -6,5 +6,19 @@ static void Main()
         const string fullName = "Просвиров Дмитрий Анатольевич";
         const string group = "ИСП-241";
         const string data = "24.09.2026";
+
+        bool Run = true;
+        while (Run)
+        {
+            Console.WriteLine("\nВыберите действие:");
+            Console.WriteLine("1 — Показать ФИО");
+            Console.WriteLine("2 — Показать группу");
+            Console.WriteLine("3 — Показать дату и время");
+            Console.WriteLine("4 — Выход");
+            Console.Write("Введите номер пункта: ");
+
+            string userInput = Console.ReadLine();
+
+        }
     }
 }
