a9053b068c92f5ed50b32f34ba96c3d436f332f7
Virpitz <dmitri.prosvirov@mail.ru>
2026-09-24 09:31:13 +0300

    step6

--- server/program.cs
+++ server/program.cs
@@ -3,7 +3,8 @@ class TextProcessor {
 static void Main()
     {
         Console.WriteLine("Доброго времени суток!");
-        const string fullName = "Просвиров Дмитрий Анатольевич";     
-        
+        const string fullName = "Просвиров Дмитрий Анатольевич";
+        const string group = "ИСП-241";
+        const string data = "24.09.2026";
     }
 }
